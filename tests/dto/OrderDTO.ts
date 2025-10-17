1acfd5c4ed6671bd2a38b6f1acf9d3865e7bab10
Martinkrk <gerstmanmartin@gmail.com>
2025-10-17 13:38:45 +0300

    fix: prettier changes

--- tests/dto/OrderDTO.ts
+++ tests/dto/OrderDTO.ts
@@ -1,12 +1,12 @@
-import { expect } from '@playwright/test'
+import { expect } from "@playwright/test";
 
 export class OrderDTO {
-  status: string
-  courierId: number
-  customerName: string
-  customerPhone: string
-  comment: string
-  id: number
+  status: string;
+  courierId: number;
+  customerName: string;
+  customerPhone: string;
+  comment: string;
+  id: number;
 
   private constructor(
     status: string,
@@ -16,27 +16,27 @@ export class OrderDTO {
     comment: string,
     id: number,
   ) {
-    this.status = status
-    this.courierId = courierId
-    this.customerName = customerName
-    this.customerPhone = customerPhone
-    this.comment = comment
-    this.id = id
+    this.status = status;
+    this.courierId = courierId;
+    this.customerName = customerName;
+    this.customerPhone = customerPhone;
+    this.comment = comment;
+    this.id = id;
   }
 
   static createOrderWithRandomData(): OrderDTO {
     return new OrderDTO(
-      'OPEN',
+      "OPEN",
       Math.floor(Math.random() * 100),
-      'John Doe',
-      '+123456789',
-      'Urgent order',
+      "John Doe",
+      "+123456789",
+      "Urgent order",
       Math.floor(Math.random() * 100),
-    )
+    );
   }
 
   static checkServerResponse(order: OrderDTO): void {
-    expect.soft(order.id).not.toBeNull()
-    expect.soft(order.customerName.length).toBeGreaterThan(0)
+    expect.soft(order.id).not.toBeNull();
+    expect.soft(order.customerName.length).toBeGreaterThan(0);
   }
 }
